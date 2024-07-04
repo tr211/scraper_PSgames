2f670e9915cca91ea6fd783c6bc7fabb4ea2e12c
Andrei Gavrilov <115535137+tr211@users.noreply.github.com>
2024-07-04 13:16:10 +0200

    Add files via upload

--- fastAPI.py
+++ fastAPI.py
@@ -1,12 +1,16 @@
+import os
 from fastapi import FastAPI
 import uvicorn
-from pars_countri import scraperPS
+from pars_countri import json_to_dict ,scraperPS, Game
 
 app = FastAPI()
+pythonfile = 'country.json'
+fi = os.path.abspath(pythonfile)
 
 @app.get('/show_table/{game_name}')
-def show_table(game_name):
-    list_of_games = scraperPS(game_name)
+def show_table(game_name)->Game:
+    countries = json_to_dict(fi)
+    list_of_games = scraperPS(game_name, countries)
     scrap_dicts = [game_name.dict() for game_name in list_of_games]
 
     return scrap_dicts
--- pars_countri.py
+++ pars_countri.py
@@ -1,48 +1,36 @@
+import os
 import json
+import re
 from pydantic import BaseModel
 import requests
 from bs4 import BeautifulSoup as bs
 
-'''
-for lokal json file 
-variable "game" need uncomment
-'''
 # game: str = input('enter game: ')
+class Game(BaseModel):
+    country: str
+    title: str
+    curency: str
+    price: str
+    publisher: str
 
-def scraperPS(game: str)->list:
-    '''
-    function retern list of objects with 
-    price from all countries realetet PS store
-    '''      
-    countries_dict = {
-    'us': 'United States', 'ca': 'Canada', 'gb': 'United Kingdom',
-      'de': 'Germany', 'fr': 'France', 'it': 'Italy', 'es': 'Spain',
-        'jp': 'Japan', 'au': 'Australia', 'nz': 'New Zealand', 'br': 'Brazil',
-          'mx': 'Mexico', 'kr': 'South Korea', 'hk': 'Hong Kong', 'tw': 'Taiwan',
-            'sg': 'Singapore', 'my': 'Malaysia', 'id': 'Indonesia', 'th': 'Thailand',
-              'ph': 'Philippines', 'in': 'India', 'sa': 'Saudi Arabia', 'ae': 'United Arab Emirates',
-                'za': 'South Africa', 'ru': 'Russia', 'tr': 'Turkey', 'pl': 'Poland', 'no': 'Norway', 'se': 'Sweden',
-                  'dk': 'Denmark', 'fi': 'Finland', 'nl': 'Netherlands', 'be': 'Belgium', 'lu': 'Luxembourg',
-                    'at': 'Austria', 'ch': 'Switzerland', 'pt': 'Portugal', 'gr': 'Greece', 'ie': 'Ireland',
-                      'hu': 'Hungary', 'cz': 'Czech Republic', 'sk': 'Slovakia', 'bg': 'Bulgaria', 'ro': 'Romania',
-                        'hr': 'Croatia', 'si': 'Slovenia', 'il': 'Israel', 'cl': 'Chile', 'ar': 'Argentina',
-                          'co': 'Colombia', 'pe': 'Peru', 'uy': 'Uruguay'
-                          }
+# pythonfile = 'country.json'
+# fi = os.path.abspath(pythonfile)
+# countries_dict = {}
+
+def json_to_dict(fi)-> dict:
+   with open(fi, 'r') as countries_dict:
+      return json.load(countries_dict)
+
+
+def scraperPS(game: str, countries_dict: dict)->list:
     
     search_game = game.replace(' ','-')
-    
-
-    class Game(BaseModel):
-        country: str
-        title: str
-        price: str
-        publisher: str
-        
     game_list: list = []
+    # game_price_list: list = []
+    
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'\
                 'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0'\
                   'Safari/537.36'}
-
 
     for k, v in countries_dict.items():
         url: str = f'https://www.playstation.com/en-{k}/games/{search_game}/'
@@ -58,30 +46,24 @@ def scraperPS(game: str)->list:
         res = soupe.find_all('div', {'class':"box game-hero__title-content"})
 
         for elements in res:
-            game_titl = elements.find('h1', class_='game-title')
-            if game_titl:
-                game_title = game_titl.text.strip()
-                game_pris = elements.find('span', class_='psw-l-line-left psw-l-line-wrap')
-                if game_pris:
-                  game_price = game_pris.text.strip()
-                  publisher_elem = elements.find('div', class_='publisher')
-                  if publisher_elem:
-                    publisher = publisher_elem.text.strip()
-                    game_list.append(Game(country=v, title=game_title, price=game_price, publisher=publisher))
+            game_title_elem = elements.find('h1', class_='game-title')
+            if game_title_elem:
+                game_title = game_title_elem.text.strip()
+                game_price_elem = elements.find('span', class_='psw-l-line-left psw-l-line-wrap')
+                if game_price_elem:
+                    game_price = game_price_elem.text.strip()
+                    match = re.match(r'([\£\$\€])([\d,]+\.?\d*)([a-zA-Z]?)', game_price)
+                    if match:
+                        currency = match.group(1) + match.group(3)
+                        price = match.group(2)
+                        publisher_elem = elements.find('div', class_='publisher')
+                        if publisher_elem:
+                            publisher = publisher_elem.text.strip()
+                            game_list.append(Game(country=v, title=game_title, game_currency=currency, price=price, publisher=publisher))
 
-    
-             
     return game_list
-                
-if __name__== '__main__':
-    pass
-# '''
-# if need lokal json file with game
-# uncomment code below
-# '''
-#   scrap = scraperPS(game)
-#   scrap_dicts = [game.dict() for game in scrap]
 
-# with open('games_table.json', 'w') as game_fi:
-#     json.dump(scrap_dicts, game_fi, indent=4)
-# print('Data has bin writen')
+if __name__=='__main__':
+  pass
+ 
+  
