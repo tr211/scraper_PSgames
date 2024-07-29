1b43e54aa803c2af81c4c7056d87d9499a655627
Andrei Gavrilov <tr211@mail.ru>
2024-07-29 17:12:22 +0200

    fix:/[search games] lover characters

--- ps_store_repository.py
+++ ps_store_repository.py
@@ -11,7 +11,8 @@ class Game(BaseModel):
     publisher: str
 
 def find_ps4_prices(game: str, countries_dict: dict)->list:
-    search_game = game.replace(' ','-')
+    game_html_format = game.replace(' ','-')
+    search_game = game_html_format.lower()
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'\
         'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0'\
