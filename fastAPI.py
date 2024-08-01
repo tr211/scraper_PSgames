4e91376843a72a4288267924535796f674923af5
Andrei Gavrilov <tr211@mail.ru>
2024-08-01 14:55:56 +0200

    add:new rout

--- fastAPI.py
+++ fastAPI.py
@@ -1,9 +1,9 @@
-import re
 from fastapi import FastAPI
 import uvicorn
 import ps_store_repository 
 import names_registry
 import file_manager
+import glob_ps_stor_search
 
 app = FastAPI()
 
@@ -12,6 +12,11 @@ def country_prices(game_name)->list[ps_store_repository.Game]:
     language_to_country = file_manager.read_countries_languages_file()
     list_of_prices = ps_store_repository.find_ps4_prices(game_name, language_to_country)
     return list_of_prices
+
+@app.get('/general-search/{game_name}')
+def general_searcher(game_name)->list[str]:
+    list_of_game_titles = glob_ps_stor_search.searcher_name_games(game_name)
+    return list_of_game_titles
 
 @app.get('/ps4-names')
 def ps4_games_names() -> list[str]:
