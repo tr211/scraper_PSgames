e74f94cbf80cd90a40934a2840f0e6c7049a4b9d
Andrei Gavrilov <tr211@mail.ru>
2024-08-03 12:44:20 +0200

    refactoring

--- fastAPI.py
+++ fastAPI.py
@@ -4,6 +4,8 @@ import ps_store_repository
 import names_registry
 import file_manager
 import glob_ps_stor_search
+import test
+
 
 app = FastAPI()
 
@@ -22,6 +24,13 @@ def general_searcher(game_name)->list[str]:
 def ps4_games_names() -> list[str]:
     names_list = names_registry.get_all_ps4_games()
     return names_list
+
+@app.get('/test/{game}')
+def test_funk(game)->list[dict]:
+    test_d = test.read_countries_languages_file()
+    game_info_df = test.find_ps4_prices(game, test_d)
+    return game_info_df
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
