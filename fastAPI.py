3f53ce29b60352da7ecfba440bfba2a6732ff777
Andrei Gavrilov <tr211@mail.ru>
2024-08-05 16:58:03 +0200

    remove symbols

--- fastAPI.py
+++ fastAPI.py
@@ -3,6 +3,9 @@ import uvicorn
 import ps_store_repository 
 import names_registry
 import file_manager
+import glob_ps_stor_search
+import test
+
 
 app = FastAPI()
 
@@ -12,10 +15,22 @@ def country_prices(game_name)->list[ps_store_repository.Game]:
     list_of_prices = ps_store_repository.find_ps4_prices(game_name, language_to_country)
     return list_of_prices
 
+@app.get('/general-search/{game_name}')
+def general_searcher(game_name)->list[str]:
+    list_of_game_titles = glob_ps_stor_search.searcher_name_games(game_name)
+    return list_of_game_titles
+
 @app.get('/ps4-names')
-def ps4_games_names()->list[str]:
+def ps4_games_names() -> list[str]:
     names_list = names_registry.get_all_ps4_games()
-    return names_list 
+    return names_list
+
+@app.get('/test/{game}')
+def test_funk(game)->list[dict]:
+    test_d = test.read_countries_languages_file()
+    game_info_df = test.find_ps4_prices(game, test_d)
+    return game_info_df
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
---
+++ glob_ps_stor_search.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+import time
+
+def searcher_name_games(game: str) -> list:
+    game_html_format = game.replace(' ', '+')
+    search_game = game_html_format.lower()
+    apostrophe = ["'", "®"]
+    for symbol in apostrophe:
+        search_game = search_game.replace(symbol, '')
+
+    # Setup Selenium WebDriver
+    options = Options()
+    options.headless = True  # Run in headless mode
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+
+    url = f'https://www.playstation.com/en-us/search/?q={search_game}&category=games'
+    driver.get(url)
+    
+    # Wait for the page to load
+    time.sleep(5)
+
+    # Extract the game titles
+    game_name_list = []
+    try:
+        title_elements = driver.find_elements(By.CLASS_NAME, 'search-results__tile__content-title')
+        for title_element in title_elements:
+            title = title_element.text.strip()
+            if title:
+                game_name_list.append(title)
+    except Exception as e:
+        print(f'Error extracting titles: {e}')
+    finally:
+        driver.quit()
+
+    return game_name_list
+
