01b25f4cc9b85d63c9be6133a92b08a277f3cd64
Andrei Gavrilov <tr211@mail.ru>
2024-08-06 17:57:39 +0200

    add rout for new scraper

--- fastAPI.py
+++ fastAPI.py
@@ -3,6 +3,7 @@ import uvicorn
 import ps_store_repository 
 import names_registry
 import file_manager
+import test
 
 app = FastAPI()
 
@@ -16,6 +17,12 @@ def country_prices(game_name)->list[ps_store_repository.Game]:
 def ps4_games_names()->list[str]:
     names_list = names_registry.get_all_ps4_games()
     return names_list 
+
+@app.get('/test/{game}')
+def test_funk(game)->list[dict]:
+    test_d = test.read_countries_languages_file()
+    game_info_df = test.find_ps4_prices(game, test_d)
+    return game_info_df
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
